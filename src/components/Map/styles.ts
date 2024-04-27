65d566d01af9ecd0b9f90397b9f73127a529a750
Дрійко КваКва <kupchak.a.m@gmail.com>
2024-04-27 20:52:55 +0300

    improved map styled

--- src/components/Map/styles.ts
+++ src/components/Map/styles.ts
@@ -2,7 +2,7 @@ import { MapContainer } from "react-leaflet";
 import styled from "styled-components";
 
 export const MapContainerStyled = styled(MapContainer)`
-  height: 400px;
+  min-height: 400px;
   ${({ theme }) =>
     theme.mq({
       width: ["100%", "100%", "60%", "60%"],
